8fb8c75a4f12d6b7fead66247f80d9c05202e30e
Shuho Ohwada <mr.kopites.tit@gmail.com>
2019-06-16 02:24:34 +0900

    [Update] add blank

--- msatubemap/views.py
+++ msatubemap/views.py
@@ -14,6 +14,7 @@ class GraphFromEggNOG():
     def on_post(self, req, resp, nogname):
         resp.media = {"hoge": nogname}
 
+
 api.add_route("/browser", Browser)
 api.add_route("/graph/custom", GraphFromCustom)
 api.add_route("/graph/eggnog/{nogname}", GraphFromEggNOG)
